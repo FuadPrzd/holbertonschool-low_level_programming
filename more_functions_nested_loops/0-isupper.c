906066a80dd0fb5feffee3cf3790f8445d455bdc
Fuad <fuadpirizade7@gmail.com>
2023-10-22 10:00:25 -0700

    cr7

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,22 @@
-#include "holberton.h"
+#include <stdio.h>
 
-/**
-  * _isupper - Check if a letter is upper
-  * @x: The number to be checked
-  *
-  * Return: 1 for upper letter or 0 for any else
-  */
-int _isupper(int x)
-{
-	if (x >= 65 && x <= 90)
-	{
-		return (1);
-	}
+int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
-	return (0);
+int main() {
+    char character = 'A'; // Change this to the character you want to check
+    int result = _isupper(character);
+
+    if (result) {
+        printf("%c is an uppercase character.\n", character);
+    } else {
+        printf("%c is not an uppercase character.\n", character);
+    }
+
+    return 0;
 }
