91fa5c30732f1d3cff22da0b7decc441a59a452f
Fuad <fuadpirizade7@gmail.com>
2023-10-22 10:09:57 -0700

    cr7

--- more_functions_nested_loops/main_0.c
+++ more_functions_nested_loops/main_0.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; ++c)
-		printf("%c: %d\n", c, _isdigit(c));
+	for (c = 'A'; c <= 'Z'; ++c)
+		printf("%c: %d\n", c, _isupper(c));
 	return (0);
 }
